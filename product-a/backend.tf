a3a60ec950fd7eee543635e27426bb5a8fca1600
youngchunse <young.chun.se@gmail.com>
2020-08-19 11:00:33 -0400

    fixed name for dynamodb table

--- product-a/backend.tf
+++ product-a/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket               = "terraform-state-dev-application"
-    dynamodb_table       = "terraform-state-dev-applicationlock"
+    dynamodb_table       = "terraform-state-dev-application-lock"
     key                  = "dev/us-east-1/landing-zone"
     region               = "us-east-1"
   }
