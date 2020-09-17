d4a90c99583e7eafcbf4f3554a3eefda73dd9da2
youngchunse <young.chun.se@gmail.com>
2020-09-17 11:10:41 -0400

    changed a comma

--- network/main.tf
+++ network/main.tf
@@ -113,7 +113,7 @@ resource "aws_subnet" "public_subnet" {
   map_public_ip_on_launch = var.map_public_ip_on_launch
   tags = map(
     "Name", "${var.tags}-public_subnet",
-    "kubernetes.io/cluster/${var.cluster_name}", "shared"
+    "kubernetes.io/cluster/${var.cluster_name}", "shared",
     "kubernetes.io/role/elb", 1
   )
 }
