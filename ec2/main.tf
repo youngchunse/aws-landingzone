bfdbf1a42b030e29c86afab79059c3d4d817e22c
youngchunse <young.chun.se@gmail.com>
2020-09-17 15:36:43 -0400

    test

--- ec2/main.tf
+++ ec2/main.tf
@@ -61,5 +61,7 @@ resource "aws_key_pair" "this" {
   key_name        = var.key_name
   public_key      = tls_private_key.tls_key.public_key_openssh
 
-  tags = var.tags
+  tags =  {
+    Name = var.tags
+  }
 }
