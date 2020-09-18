8d5a66e4732aa536b0de8906f6b23a9859cf48e2
youngchunse <young.chun.se@gmail.com>
2020-09-18 14:56:06 -0400

    testing

--- ec2/main.tf
+++ ec2/main.tf
@@ -53,13 +53,14 @@ resource "aws_instance" "instance" {
 
 resource "tls_private_key" "tls_key" {
   algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
 resource "aws_key_pair" "this" {
   count = var.create_key_pair ? 1 : 0
 
   key_name        = var.key_name
-  public_key      = var.public_key
+  public_key      = tls_private_key.tls_key.public_key_openssh
 
   tags =  {
     Name = var.tags
