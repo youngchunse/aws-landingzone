602b960f2e7ab3e0a6a569de5cddd19100ce86a7
youngchunse <young.chun.se@gmail.com>
2020-10-19 11:11:22 -0400

    changed output variables

--- app/outputs.tf
+++ app/outputs.tf
@@ -14,18 +14,18 @@ output "bastion_sg_id" {
   value = module.vpc1.bastion_security_group_id[0]
 }
 
-output "vpc_id" {
+output "vpc2_id" {
   value = module.vpc2.vpc_id
 }
 
-output "private_subnet_id" {
+output "private2_subnet_id" {
   value = module.vpc2.private_subnets
 }
 
-output "database_subnet_id" {
+output "database2_subnet_id" {
   value = module.vpc2.database_subnets
 }
 
-output "bastion_sg_id" {
+output "bastion2_sg_id" {
   value = module.vpc2.bastion_security_group_id[0]
 }
